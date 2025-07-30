54353e56e9da32eef7966d7853a9da378cb7bcdc
Teddy <teddyantepuesto@gmail.com>
2025-07-30 15:15:41 +1300

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import { FaHome } from "react-icons/fa";
+import { RiContactsBook3Fill } from "react-icons/ri";
+import { MdImportContacts } from "react-icons/md";
+import { BsDiagram2Fill } from "react-icons/bs";
 import '../styles/Header.css'
 const Header = () => {
   return (
     <div className='header-container'>
       <nav>
         <ul className="header-nav">
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Contact</li>
+          <li><FaHome className='header-icon' /></li>
+          <li><MdImportContacts className='header-icon' /></li>
+          <li><BsDiagram2Fill className='header-icon' /></li>
+          <li><RiContactsBook3Fill className='header-icon' /></li>
         </ul>
       </nav>
     </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,13 +5,13 @@ const Skills = () => {
     <div className='skills-container'>
       <h2>Skills</h2>
       <ul>
-        <li><img src="../public/jslogo.png" alt="" /></li>
-        <li><img src="../public/Bootstraplogo.png" alt="" /></li>
-        <li><img src="../public/CSS.png" alt="" /></li>
-        <li><img src="../public/htmllogo.png" alt="" /></li>
-        <li><img src="../public/git.png" alt="" /></li>
-        <li><img src="../public/php.png" alt="" /></li>
-        <li><img src="../public/tailwind.png" alt="" /></li>
+        <li><img src="../jslogo.png" alt="" /></li>
+        <li><img src="../Bootstraplogo.png" alt="" /></li>
+        <li><img src="../CSS.png" alt="" /></li>
+        <li><img src="../htmllogo.png" alt="" /></li>
+        <li><img src="../git.png" alt="" /></li>
+        <li><img src="../php.png" alt="" /></li>
+        <li><img src="../tailwind.png" alt="" /></li>
       </ul>
       <button className='know-more-button'>Know me more</button>
     </div>

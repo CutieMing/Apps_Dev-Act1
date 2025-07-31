ac3df722a9f04ff3c39aedecb89fd3e4a128fbaf
Paul <Paul@gmail.com>
2025-07-31 13:50:57 +1300

    profile

--- src/components/ProfilePicture.jsx
+++ src/components/ProfilePicture.jsx
@@ -3,7 +3,7 @@ import '../styles/Profile-Picture.css'
 const ProfilePicture = () => {
   return (
     <div className='profile-picture-container'>
-      <img src="../public/profile.jpg" alt="Profile" />
+      <img src="../profile.jpg" alt="Profile" />
      <div className='profile-text'>
        <h3>Hi! I'm Teds a Junior Web Developer.</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.</p>
